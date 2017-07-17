b307e935fef8a910927b5a99016c04808d7c36d5
Steve <ssa3512@gmail.com>
2017-07-17 16:12:21 -0400

    initial commit

---
+++ StorjDotNet/Storj.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorjDotNet
+{
+    public class Storj
+    {
+        [DllImport("libstorj-0.dll")]
+        public static extern int storj_mnemonic_generate(int strength, ref IntPtr buffer);
+
+        public Storj()
+        {
+        }
+
+        public string generateMnemonic(int strength)
+        {
+            IntPtr ptr = new IntPtr(Int64.MaxValue);
+            int success = storj_mnemonic_generate(strength, ref ptr);
+
+            if (success == 1)
+            {
+                return Marshal.PtrToStringAnsi(ptr);
+            }
+            return string.Empty;            
+        }
+    }
+}
---
+++ StorjTests/StorjTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using StorjDotNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StorjTests
+{
+    [TestClass]
+    public class StorjTests
+    {
+        [TestMethod]
+        public void ShouldReturnMnemonic_128()
+        {
+            Storj storj = new Storj();
+            string mnemonic = storj.generateMnemonic(128);
+
+            Assert.IsFalse(string.IsNullOrEmpty(mnemonic), "Mnemonic should not be null or empty");
+            Assert.IsTrue(mnemonic.Split(' ').Length == 12, "Mnemonic should be 12 words");
+        }
+
+        [TestMethod]
+        public void ShouldReturnMnemonic_256()
+        {
+            Storj storj = new Storj();
+            string mnemonic = storj.generateMnemonic(256);
+
+            Assert.IsFalse(string.IsNullOrEmpty(mnemonic), "Mnemonic should not be null or empty");
+            Assert.IsTrue(mnemonic.Split(' ').Length == 24, "Mnemonic should be 24 words");
+        }
+    }
+}
